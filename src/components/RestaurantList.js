8e1c84ac5734e273fe29406af3132d6d9e14060f
Jose Elias <josejosephelias@gmail.com>
2024-10-27 22:23:08 +0200

    only edited the food data to put the type of food in an array

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../gallery/style/RestaurantList.css";
 import data from "../FoodData";
-import RestaurantCard from "./RestaurantCard"
+import RestaurantCard from "./RestaurantCard";
 
 const RestaurantList = ({ count, typeFilter }) => {
-  const filteredData = typeFilter ? data.filter((r) => r.type === typeFilter) : data; // Show all restaurants if no typeFilter is provided
+  const filteredData = typeFilter
+    ? data.filter((r) => r.type === typeFilter)
+    : data; // Show all restaurants if no typeFilter is provided
 
   const displayedData = count ? filteredData.slice(0, count) : filteredData; // Show all restaurants if no count is provided
 
@@ -13,7 +15,12 @@ const RestaurantList = ({ count, typeFilter }) => {
     <div>
       <div className="restaurants-list">
         {displayedData.map((r) => (
-          <RestaurantCard key={r.id} favorite={r.favorite} {...r} />
+          <RestaurantCard
+            key={r.id}
+            favorite={r.favorite}
+            {...r}
+            type={r.type.join(", ")}
+          />
         ))}
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../gallery/style/Home.css';
+import React from "react";
+import "../gallery/style/Home.css";
 import Gif1 from "../gallery/images/gif1.gif";
 import Gif2 from "../gallery/images/gif2.gif";
 import Gif3 from "../gallery/images/gif3.gif";
@@ -13,29 +13,43 @@ const Home = () => {
         <div className="status">
           <div className="orders-status">
             <div className="green-status">Green</div>
-            <div className="sub-status">10 more orders by October 31 to reach Gold.</div>
+            <div className="sub-status">
+              10 more orders by October 31 to reach Gold.
+            </div>
           </div>
-          <div className="points-status">
-            80K Points
-          </div>
+          <div className="points-status">80K Points</div>
         </div>
 
-        <div className='Hero'>
-          <div className='hero-details-section'>
-            <div className='hero-text'>
-              <h1 className='hero-title'>Fast and Easy Delivery</h1>
-              <p className='hero-desc'>Get what you want, the way you want it,<br/>exactly when you want it and pay by card or cash.<br/> 
-              Delivery has never been this easy.<br/><br/>Watch your order every step of the way,<br/>from the minute you place it, to when it’s outside your door.</p>
+        <div className="Hero">
+          <div className="hero-details-section">
+            <div className="hero-text">
+              <h1 className="hero-title">Fast and Easy Delivery</h1>
+              <p className="hero-desc">
+                Get what you want, the way you want it,
+                <br />
+                exactly when you want it and pay by card or cash.
+                <br />
+                Delivery has never been this easy.
+                <br />
+                <br />
+                Watch your order every step of the way,
+                <br />
+                from the minute you place it, to when it’s outside your door.
+              </p>
             </div>
-            <div className='download-tags'>
-              <img className='download' src={AppStore} alt='App Store'></img>
-              <img className='download' src={GooglePLay} alt='Google PLay'></img>
+            <div className="download-tags">
+              <img className="download" src={AppStore} alt="App Store"></img>
+              <img
+                className="download"
+                src={GooglePLay}
+                alt="Google PLay"
+              ></img>
             </div>
           </div>
-          <div className='gif-container'>
-            <img className='gif' src={Gif1} alt="gif1" />
-            <img className='gif' src={Gif3} alt="gif3" />
-            <img className='gif' src={Gif2} alt="gif2" />
+          <div className="gif-container">
+            <img className="gif" src={Gif1} alt="gif1" />
+            <img className="gif" src={Gif3} alt="gif3" />
+            <img className="gif" src={Gif2} alt="gif2" />
           </div>
         </div>
       </div>
